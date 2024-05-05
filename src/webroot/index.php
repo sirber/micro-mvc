aee862a3ce74d2d5fa6783a2dd678b1be8020aa4
Stephane Berube <sirber@protonmail.com>
2024-05-05 09:39:59 -0400

    cleanup

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -2,9 +2,6 @@
 
 require_once __DIR__ . '/../bootstrap.php';
 
-use App\Controllers\HomeController;
-use App\Controllers\UserController;
-
 $request = $_SERVER["REQUEST_URI"];
 $parts = explode('/', $request);
 $endpoint = '/' . $parts[1];
